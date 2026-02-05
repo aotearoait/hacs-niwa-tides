0408e5f02edd661afd42c243619a4b09f68e4f11
aotearoait <33107115+aotearoait@users.noreply.github.com>
2026-02-05 16:01:30 +1300

    Revert to old working code

--- custom_components/niwa_tides/__init__.py
+++ custom_components/niwa_tides/__init__.py
@@ -1,1 +1,9 @@
-# see sensor.py
+"""NIWA Tides integration."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup(hass, config):
+    """Set up the NIWA Tides component."""
+    _LOGGER.error("NIWA TIDES COMPONENT LOADING - THIS IS A TEST MESSAGE")
+    return True
